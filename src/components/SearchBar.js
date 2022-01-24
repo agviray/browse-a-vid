388a4976b21ec8ed76802735e9ee808446d1c9cf
Allan Viray <allang.v.dev@gmail.com>
2022-01-24 17:14:07 -0600

    Create SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <div>This is the SearchBar component</div>
+    );
+  }
+}
+
+export default SearchBar;
