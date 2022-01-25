15f4a0bf082d4ebc217f8f48eb3c03dbfcb72fd2
Allan Viray <allang.v.dev@gmail.com>
2022-01-24 19:00:36 -0600

    Assign event handler for input onChange event

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
+  state = { term: '' };
 
   render() {
+    console.log(this.state.term);
     return(
       <div className="searchbar">
-        <form className="searchbar-form" onSubmit="">
+        <form className="searchbar-form">
           <div className="searchbar-field">
             <label className="searchbar-label">Type something</label>
-            <input className="searchbar-input" type="text" />
+            <input 
+              type="text"
+              className="searchbar-input" 
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
