a8674c402c7049d30ca02b369e39a0c2711db5df
Allan Viray <allang.v.dev@gmail.com>
2022-01-24 19:09:05 -0600

    Define event handler for form onSubmit event
    - Initial definition of onSubmit's event handler.
    
    - The handler will prevent the form's default behavior, if the user
      hits 'Enter' after typing a search term in the input.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,18 +3,22 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     console.log(this.state.term);
     return(
       <div className="searchbar">
-        <form className="searchbar-form">
+        <form className="searchbar-form" onSubmit={this.onFormSubmit}>
           <div className="searchbar-field">
             <label className="searchbar-label">Type something</label>
             <input 
               type="text"
               className="searchbar-input" 
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
+              onChange={(event) => this.setState({ term: event.target.value })}
             />
           </div>
         </form>
