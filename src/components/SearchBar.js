f1b998c75707a60d33b5a02af4f06bdeca510e92
Allan Viray <allang.v.dev@gmail.com>
2022-01-24 18:27:28 -0600

    Set up initial structure of SearchBar JSX

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+
   render() {
     return(
-      <div>This is the SearchBar component</div>
+      <div className="searchbar">
+        <form className="searchbar-form" onSubmit="">
+          <div className="searchbar-field">
+            <label className="searchbar-label">Type something</label>
+            <input className="searchbar-input" type="text" />
+          </div>
+        </form>
+      </div>
     );
   }
-}
+};
 
 export default SearchBar;
