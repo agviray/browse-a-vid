e402d9ec761be55d9477f51ca821839762633a56
Allan Viray <allang.v.dev@gmail.com>
2022-01-26 17:36:27 -0600

    Create onTermSubmit and pass as props to SearchBar
    - onTermSubmit is the callback which will be called from inside
      SearchBar's onFormSubmit function.
    
    - It must be called in order to pass the user's searched term from
      the SearchBar component, to the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import SearchBar from './SearchBar.js';
 import './SearchBar.css';
 
 class App extends React.Component {
+  onTermSubmit = (term) => {
+    console.log(term);
+  }
+
   render() {
     return(
       <div>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
