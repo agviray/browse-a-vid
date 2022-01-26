dd7d4ff842416316a69306b53829272437ef7c95
Allan Viray <allang.v.dev@gmail.com>
2022-01-26 14:10:49 -0600

    Create pre-configured instance of axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBex7MWkVJ3_mL7LsFGUPY1DOGTuqklA6k';
+ 
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
