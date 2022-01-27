2371045a2043232c3e0e4fcb2a9dce391764513e
Allan Viray <allang.v.dev@gmail.com>
2022-01-26 21:20:12 -0600

    Set "videos" property of state to be video data received from api
    - When the user enters a search term(s), App component's state.videos
      will update to be an array of video data, which was obtained via the
      response from the Youtube API.

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,23 @@ import youtube from '../apis/youtube';
 import './SearchBar.css';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+    
+    this.setState({ videos: response.data.items });
   }
 
   render() {
     return(
       <div>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        Your search found {this.state.videos.length} videos.
       </div>
     );
   }
