7397ab126213811222c3335ee233ae181295d57a
Allan Viray <allang.v.dev@gmail.com>
2022-01-27 21:03:35 -0600

    Reference video properties to render a thumbnail and title for each VideoItem instance

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
   return(
     <div>
-      Video List
+      <img src={video.snippet.thumbnails.medium.url} alt="" />
+      {video.snippet.title}
     </div>
   );
 }
