4cc2e993871c2901032c6d0e8fff6a224292f2a4
Allan Viray <allang.v.dev@gmail.com>
2022-01-28 17:06:08 -0600

    Create variable for embed src of clicked video and reference it as value to iframe src

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,11 +5,13 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return(
     <div>
       <div className="ui segment">
         <div className="ui embed">
-          <iframe src=""></iframe>
+          <iframe src={videoSrc}></iframe>
         </div>
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
